bcce6725ca16b62d6b72f4767e3ef7f3cff8e4e4
authithya <authithya@gmail.com>
2021-12-24 22:47:03 +0530

    authithya code

--- src/test/java/org/test/TestProject.java
+++ src/test/java/org/test/TestProject.java
@@ -8,6 +8,11 @@ public static void main(String[] args) {
 	int b=10;
 	int c=a+b;
 	System.out.println(c);
+	
+	String a1="welcome";
+	String b1="selenium";
+	String c1=b1.concat(a1);
+	System.out.println(c1);
 }
 
 }
