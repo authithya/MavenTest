efe32c6f765e5a9bf71b00eabacc49b8dafb3a77
authithya <authithya@gmail.com>
2021-12-28 22:20:17 +0530

    authithya update

--- src/test/java/org/test/TestProject.java
+++ src/test/java/org/test/TestProject.java
@@ -4,15 +4,7 @@ public class TestProject {
 	
 public static void main(String[] args) {
 	
-	int a=10;
-	int b=10;
-	int c=a+b;
-	System.out.println(c);
-	
-	String a1="welcome";
-	String b1="selenium";
-	String c1=b1.concat(a1);
-	System.out.println(c1);
+	System.out.println("aathi");
 }
 
 }
